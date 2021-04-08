64dabe2cae233cbff541d788252f2049e6be8c03
armidsinner <1986damir55@gmail.com>
2021-04-08 19:30:39 +0300

    Update Q1.rb

--- Q1.rb
+++ Q1.rb
@@ -2,13 +2,18 @@ class Station
   def initialize(name, trains = [])
       @name = name
       @trains = trains
-  end 
-  def accept_train
-   @trains.append(Train.new) 
   end
+
+  def accept_train(train_new)
+    if train_new.instans_of? Trains
+      @trains.append(train_new)
+    end
+  end
+
   def trains_on_the_station
     return (@trains)
   end
+
   def freight_trains
     for train in @trains
       if train.type == freight
@@ -17,6 +22,7 @@ class Station
     end
     return (@freight_list)
   end
+
   def passenger_trains
     for train in @trains 
       if train.type == passenger 
@@ -25,6 +31,7 @@ class Station
     end
     return(@passenger_list)
   end
+
   def send_train(train_to_remove)
     @trains.remove(train_to_remove)
 end
@@ -33,13 +40,16 @@ class Route
   def initialize(start_station, end_station, list_of_stations = [start_station, end_station] )
     @start_station = start_station
     @end_station = end_station
-  end 
+  end
+
   def add_station() 
     @list_of_stations.insert(@list_of_stations.length - 1, new_station)
   end 
+
   def remove_station(needed_station)
     @list_of_stations.remove(needed_station) 
   end
+
   def show_route
     return(@list_of_stations)
   end
@@ -53,28 +63,35 @@ class Train
     @amount_of_railways = amount_of_railways
     @speed = speed
   end
+
   def gain_speed(gain)
     @speed = @speed + gain 
   end
+
   def stop 
     @speed = 0
   end
+
   def add_railway 
     if @speed == 0 
       @amount_of_railways += 1
     end
-  end  
+  end
+
   def remove_railway 
     if @speed == 0 and @amount_of_railways > 0
       @amount_of_railways -= 1
     end
   end
-  def set_route()
-    @own_route = Route
-    @current_station = Route.start_station
-    @station_index =
-    Station.name(Route.start_station).trains.append(self)
-end
+
+  def set_route(own_route) 
+    if own_route.instans_of? Route
+      @current_station = Route.start_station
+      @station_index =
+      Station.name(Route.start_station).trains.append(self)
+    end
+  end
+
   def move_train_next
     if @current_station != @end_station  
       @current_station = Route.list_of_stations[@station_index + 1]     
@@ -86,6 +103,7 @@ end
       current_station = Route.list_of_stations[@station_index - 1]
     end 
   end
-end   
-end 
+end
+end    
+
 
